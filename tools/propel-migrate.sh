0f38f59c5de58e72c0f717b4d4b88d7e3f7fa4d8
Fabien DI Tore <fabien@ditore.ch>
2017-07-07 11:15:30 +0200

    fixed propel-migrate bash script

--- tools/propel-migrate.sh
+++ tools/propel-migrate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+DIRNAME=$(dirname $0)
+ROOT=$DIRNAME/../../../../
 
-src/vendor/propel/propel/bin/propel migrate --config-dir src/app/config/dev  -v
+$ROOT/vendor/propel/propel/bin/propel migrate --config-dir  $ROOT/app/config/dev || exit 1
+
+
+#src/vendor/propel/propel/bin/propel migrate --config-dir src/app/config/dev  -v
